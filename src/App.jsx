590f03475fddb6d1e366a76dc5d3eae14a3bf586
elitecoder05 <varma.gurijala@gmail.com>
2025-11-15 16:12:22 +0530

    kokokokok

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import { useState } from 'react';
 import LocationMap from './components/Map';
 import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+const defaultIcon = new L.Icon.Default();
 
 const LOCATION_TYPES = {
   SOURCE: 'source',
